f5c0a7806499643a8783c6d696c832b2b0fe5858
Anthony Stolp <anthony.stolp@gmail.com>
2023-04-10 17:27:32 -0400

    ready to add schemas

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
+import schemas from "./sanity/schemas";
 
 const config = defineConfig({
   projectId: "5d6920vu",
@@ -8,6 +9,7 @@ const config = defineConfig({
   apiVersion: "2023-04-10",
   basePath: "/admin",
   plugins: [deskTool()],
+  shcema: { types: schemas },
 });
 
 export default config;
