21d5020a18bd9b196863f751588e4a3c08c469da
Anthony Stolp <47982927+AJStolp@users.noreply.github.com>
2023-05-03 17:08:51 -0400

    Earn wip (#6)
    * refactor pages
    
    * comment out

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,10 +1,12 @@
-import { getTable, getHero } from "@/sanity/sanity-utils";
+import { getTable, getHero, getJumbotron } from "@/sanity/sanity-utils";
 import Hero from "./components/hero/hero";
 import Table from "./components/table/table";
+import Jumbotron from "./components/jumbotron/jumbotron";
 
 export default async function Home() {
   const hero = await getHero();
   const table = await getTable();
+  // const jumbotron = await getJumbotron();
 
   return (
     <>
---
+++ types/jumbotron-with-cards.ts
@@ -0,0 +1,8 @@
+import { PortableTextBlock } from "sanity";
+
+export type jumbotroncards = {
+  _id: string;
+  _createdAt: string;
+  heading: string;
+  content: PortableTextBlock;
+};
